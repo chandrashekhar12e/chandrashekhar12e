b955157a8b20d8cfd9cb93e00d5711147de23e92
chandrashekhar <101011058+chandrashekhar12e@users.noreply.github.com>
2022-05-29 11:36:06 +0530

    Update mathq1.py

--- mathq1.py
+++ mathq1.py
@@ -1,5 +1,6 @@
-import os
-if os.path.exists("demofile.txt"):
-  os.remove("demofile.txt")
-else:
-  print("The file does not exist")
+import math
+r = 4
+pie = math.pi
+
+# area of the circle
+print(pie * r * r)
